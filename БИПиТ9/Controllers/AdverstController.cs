5791fd6f9b14b4188f7c007c04bc0f89b64bff2e
Augen-Auf <an_lesiv@mail.ru>
2020-12-16 17:07:00 +0500

    added БИПиТ9

---
+++ БИПиТ9/Controllers/AdverstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace БИПиТ9.Controllers
+{
+    public class AdverstController : Controller
+    {
+        AdverstContext db = new AdverstContext();
+
+        public ActionResult Client()
+        {
+            return View(db.Client.ToList());
+        }
+        public ActionResult Service()
+        {
+            return View(db.Service.ToList());
+        }
+        public ActionResult Order()
+        {
+            return View(db.OrderView.ToList());
+        }
+
+    }
+}
---
+++ БИПиТ9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="БИПиТ9.MvcApplication" Language="C#" %>
